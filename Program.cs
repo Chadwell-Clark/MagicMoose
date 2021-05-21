9928d9ae59422440d7c7a769b8086547e675cc11
Chadwell Clark <wcclark23@comcast.net>
2021-05-21 18:18:30 -0500

    Phase 2 & 3

--- Program.cs
+++ Program.cs
@@ -4,8 +4,28 @@ Main();
 
 void Main()
 {
-
-    //Let the moose speak
+    //        string[] answers =
+    //     { "As I see it, yes.",
+    // "Ask again later.",
+    // "Better not tell you now.",
+    // "Cannot predict now.",
+    // "Concentrate and ask again.",
+    // "Don’t count on it.",
+    // "It is certain.'",
+    // "It is decidedly so.",
+    // "Most likely.",
+    // "My reply is no.",
+    // "My sources say no.",
+    // "Outlook not so good.",
+    // "Outlook good.",
+    // "Reply hazy, try again.",
+    // "Signs point to yes.",
+    // "Very doubtful.",
+    // "Without a doubt.",
+    // "Yes.",
+    // "Yes – definitely.",
+    // "You may rely on it." };
+    //     Random r = new Random();
     MooseSays("Welcome weary bushwacker. I am the Magical Moose of Mystery!");
     Question();
 
@@ -45,16 +65,42 @@ void MooseSays(string message)
 
 void Question()
 {
-    Console.Write("Ask me any question and I will answer!");
+    Console.Write("Ask me any question and I will answer!  ");
+
     string inquiry = Console.ReadLine();
     Answer(inquiry);
-
+    Console.WriteLine();
 }
 
 void Answer(string inquiry)
 {
-    if (!String.IsNullOrWhiteSpace(inquiry))
+    string[] answers =
+    { "As I see it, yes.",
+"Ask again later.",
+"Better not tell you now.",
+"Cannot predict now.",
+"Concentrate and ask again.",
+"Don’t count on it.",
+"It is certain.'",
+"It is decidedly so.",
+"Most likely.",
+"My reply is no.",
+"My sources say no.",
+"Outlook not so good.",
+"Outlook good.",
+"Reply hazy, try again.",
+"Signs point to yes.",
+"Very doubtful.",
+"Without a doubt.",
+"Yes.",
+"Yes – definitely.",
+"You may rely on it." };
+    Random r = new Random();
+    if (!String.IsNullOrEmpty(inquiry))
     {
-
+        int mooseMagic = r.Next(answers.Length);
+        Console.WriteLine(answers[mooseMagic]);
+        Console.WriteLine();
+        Question();
     }
 }

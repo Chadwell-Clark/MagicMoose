9afad86373b336baaecc6ab0978527b155096397
Chadwell Clark <wcclark23@comcast.net>
2021-05-21 17:32:38 -0500

    Initial Commit Phase 1

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+Main();
+
+void Main()
+{
+
+    //Let the moose speak
+    MooseSays("Welcome weary bushwacker. I am the Magical Moose of Mystery!");
+    Question();
+
+}
+
+void MooseSays(string message)
+{
+    Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |   {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
+
+void Question()
+{
+    Console.Write("Ask me any question and I will answer!");
+    string inquiry = Console.ReadLine();
+    Answer(inquiry);
+
+}
+
+void Answer(string inquiry)
+{
+    if (!String.IsNullOrWhiteSpace(inquiry))
+    {
+
+    }
+}
